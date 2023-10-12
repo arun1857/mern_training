9c9e704538ab616cf927d448e05886628d057f57
Arun Tekwani <arun.t@taskcraft.in>
2023-10-12 11:31:28 +0530

    day 5 commit

---
+++ day5.js
@@ -0,0 +1,74 @@
+// let arr = [3,5,6,7]
+// let a = arr.reduce(getmux, 0)
+
+// function getmux(total, num){
+//     return total + num;
+// }
+
+// console.log(a);
+
+// let numbers = [45, 4, 9, 16, 25];
+// let over20 = numbers.filter(get_fil);
+
+// function get_fil(val, index, arr){
+//     return val > 20; 
+// }
+
+// console.log(over20);
+
+// let numbers = [4, 4, 9, 16, 2];
+// let over20 = numbers.some(get_fil);
+
+// function get_fil(val, index, arr){
+//     return val > 20; 
+// }
+
+// console.log(over20);
+
+
+
+
+// let numbers = [45, 40, 90, 160, 20];
+// let over20 = numbers.every(get_fil);
+
+// function get_fil(val, index, arr){
+//     return val > 20; 
+// }
+
+// console.log(over20);
+
+// let numbers = [3, 40, 90, 160, 20];
+// let over20 = numbers.find(get_fil);
+
+// function get_fil(val, index, arr){
+//     return val > 20; 
+// }
+
+// console.log(over20);
+
+// let hi = () => {console.log("Hello World");}
+
+// hi();
+
+let sum = (a,b) => {
+    return a+b;
+}
+
+console.log(sum(4,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
