19700a4020f6f1997cd8e5e37f4963ac9fdb15df
Arun Tekwani <arun.t@taskcraft.in>
2023-10-15 13:56:26 +0530

    day6 commit

--- day6.js
+++ day6.js
@@ -4,29 +4,29 @@
 // }, 3000);
 // console.log("Hello");
 
-function abc(a,b, v){
-    console.log(v(a,b));
-}
+// function abc(a,b, v){
+//     console.log(v(a,b));
+// }
 
-function add(a,b){
-    return a+b;
-}
+// function add(a,b){
+//     return a+b;
+// }
 
-abc(4,5, add);
+// abc(4,5, add);
 
-// let abc = new Promise(function(reject, resolve){
-//     let h = 10
-//     if (h == 0){
-//         resolve("kal Bihari haarege");
-//     } else {
-//         reject("zarur haarege");
-//     }
-// });
+let abc = new Promise(function(resolve, reject){
+    let h = 10
+    if (h == 0){
+        resolve("kal Bihari haarege");
+    } else {
+        reject("zarur haarege");
+    }
+});
 
-// abc.then(
-//     function(value) { console.log(value) },
-//     function(error) { console.log(error) }
-// )
+abc.then(
+    function(value) { console.log(value) },
+    function(error) { console.log(error) }
+)
 
 
 
